164a6fcd4471a7a6d17c939cd31c2e68c1fb56a3
ab3nxy <abenxy0@gmail.com>
2024-04-13 22:25:17 +0300

    Render 5

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use('/api/posts', require('./routes/api/posts'));
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static(path.join(__dirname, 'client', 'build')));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
@@ -30,4 +30,4 @@ if (process.env.NODE_ENV === 'production') {
 const port = process.env.PORT || 5000;
 
 // app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-app.listen(port, '0.0.0.0', () => console.log(`Server listening on port ${port}`));
+app.listen(port, () => console.log(`Server listening on port ${port}`));
